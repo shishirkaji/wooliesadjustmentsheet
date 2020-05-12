dfc3cc0caff3ce54d096edcf43cb3a7c0631f67d
root <root@ip-172-31-27-118.us-east-2.compute.internal>
2020-05-12 02:41:05 +0000

    from server

---
+++ client/woolies-adjustment/build/precache-manifest.fb84599008ae8798b12b485ffcc63290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e45dbb2c8ec5be3f8e1c8bf4df111e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075f58023cf0d828ba02",
+    "url": "/static/css/2.392f69cd.chunk.css"
+  },
+  {
+    "revision": "aeef0dc85586baa88dfd",
+    "url": "/static/css/main.4f821986.chunk.css"
+  },
+  {
+    "revision": "075f58023cf0d828ba02",
+    "url": "/static/js/2.bac75f4d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.bac75f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeef0dc85586baa88dfd",
+    "url": "/static/js/main.b2956ff7.chunk.js"
+  },
+  {
+    "revision": "a8af75df93c07a053cd0",
+    "url": "/static/js/runtime-main.63fb48db.js"
+  }
+]);
